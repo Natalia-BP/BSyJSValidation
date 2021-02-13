588c4e79fd7d261a74d4899be319d4e708f1e37e
Natalia-BP <nataliabarqueroperez@gmail.com>
2021-02-13 20:58:56 +0000

    mejora en reg ex de letras, ahora acepta espacios

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ submitForm.addEventListener("submit", function (event) {
     let postal = document.getElementById("postalCode");
 
     // Credit Card
+    // Regular expression for credit cards
     const creditPattern = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/
     let result = creditPattern.test(creditCard.value)
     if (!result) {
@@ -56,7 +57,7 @@ submitForm.addEventListener("submit", function (event) {
     }
 
     // First Name
-    const letter = /^[A-Za-z]+$/;
+    const letter = /^[a-zA-Z\s]*$/;
     let resultFristName = letter.test(fristName.value);
     if (!resultFristName)  {
 
